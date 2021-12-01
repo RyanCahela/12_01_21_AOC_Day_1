e6147e9c73a59ae48ad4c0601d6337bb59fa9de1
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-01 09:49:17 -0800

    it should work but doesn't, adding new file for test readings

--- day1.js
+++ day1.js
@@ -5,6 +5,28 @@ function getDepthValuesFromFile(filename) {
   return fs.readFileSync(filename).toString("utf-8").split("\n");
 }
 
-const depthsArray = getDepthValuesFromFile("depth_readings.txt");
+const depths = getDepthValuesFromFile("depth_readings.txt");
 
-console.log(depthsArray);
+console.log(depths);
+
+//create a store (variable) to keep track of the count
+let count = 0;
+
+//loop through the array
+let previousDepth = 0;
+for(let i = 0; i < depths.length; i++) {
+  if(i === 0) {
+    previousDepth = depths[i];
+    continue;
+  }
+  //count each time the current number is higher than the previous number.
+  if(depths[i] > previousDepth) {
+    count += 1;
+  }
+
+  //assign current reading to previousDepth
+  previousDepth = depths[i];
+}
+
+//output count
+console.log('count', count);
