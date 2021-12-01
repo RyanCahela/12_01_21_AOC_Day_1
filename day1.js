599ade5253215abc9d2d152cfee1687ffee1ab8d
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-01 10:41:14 -0800

    still doesn't work but reads from file async

--- day1.js
+++ day1.js
@@ -1,32 +1,38 @@
 const fs = require("fs");
 
-function getDepthValuesFromFile(filename) {
+async function getDepthValuesFromFile(filename) {
   //TODO: refactor to us async file read
-  return fs.readFileSync(filename).toString("utf-8").split("\n");
+  try {
+    const string = await fs.promises.readFile(filename, "utf-8");
+    return string.split("\n");
+  } catch(error) {
+    throw new Error(error.message);
+  }
 }
 
-const depths = getDepthValuesFromFile("depth_readings.txt");
-
-console.log(depths);
-
-//create a store (variable) to keep track of the count
-let count = 0;
-
-//loop through the array
-let previousDepth = 0;
-for(let i = 0; i < depths.length; i++) {
-  if(i === 0) {
+async function countIncreasese() {
+  const depths = await getDepthValuesFromFile("depth_readings.txt");
+  //create a store (variable) to keep track of the count
+  let count = 0;
+  
+  //loop through the array
+  let previousDepth = 0;
+  for(let i = 0; i < depths.length; i++) {
+    if(i === 0) {
+      previousDepth = depths[i];
+      continue;
+    }
+    //count each time the current number is higher than the previous number.
+    if(depths[i] > previousDepth) {
+      count += 1;
+    }
+  
+    //assign current reading to previousDepth
     previousDepth = depths[i];
-    continue;
   }
-  //count each time the current number is higher than the previous number.
-  if(depths[i] > previousDepth) {
-    count += 1;
-  }
-
-  //assign current reading to previousDepth
-  previousDepth = depths[i];
+  
+  //output count
+  console.log('count', count);
 }
 
-//output count
-console.log('count', count);
+countIncreasese();
