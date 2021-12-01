47b7e36a4afd6c34ad48489d7eb96d57b14659c3
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-01 11:11:38 -0800

    refactored to remove superfluous variables, still off by one...

--- day1.js
+++ day1.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 async function getDepthValuesFromFile(filename) {
-  //TODO: refactor to us async file read
   try {
     const string = await fs.promises.readFile(filename, "utf-8");
     return string.split("\n");
@@ -16,19 +15,11 @@ async function countIncreasese() {
   let count = 0;
   
   //loop through the array
-  let previousDepth = 0;
   for(let i = 0; i < depths.length; i++) {
-    if(i === 0) {
-      previousDepth = depths[i];
-      continue;
-    }
     //count each time the current number is higher than the previous number.
-    if(depths[i] > previousDepth) {
-      count += 1;
+    if(depths[i] > depths[i - 1]) {
+      count++;
     }
-  
-    //assign current reading to previousDepth
-    previousDepth = depths[i];
   }
   
   //output count
